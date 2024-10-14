367ae2403b117acbe43e5daf56307e38cbbd06c3
Nông Văn Thắng <52882277+ThangStar@users.noreply.github.com>
2024-10-14 23:55:52 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+  basePath: "/2048-in-react",
+  output: "export",
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
