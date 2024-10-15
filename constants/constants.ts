ff7182793b7a34472554a8c7c6a9d3b5a6094334
Nông Văn Thắng <52882277+ThangStar@users.noreply.github.com>
2024-10-15 08:06:52 +0700

    fix deploy

--- constants/constants.ts
+++ constants/constants.ts
@@ -4,7 +4,7 @@ export const VOICE_LIST: Voice[] = [
     {
         id: 1,
         name: 'Thành Phạm',
-        avatar_uri: '/image/thanh_pham.jpg',
+        avatar_uri: 'image/thanh_pham.jpg',
         idRepo: 'capleaf/viXTTS',
         voice_uri: '/voice/thanhpham.wav'
     },
@@ -12,21 +12,21 @@ export const VOICE_LIST: Voice[] = [
         id: 2,
         name: 'Cô ba Bình Dương',
         idRepo: 'capleaf/viXTTS',
-        avatar_uri: '/image/co_ba_binh_duong.jpg',
+        avatar_uri: 'image/co_ba_binh_duong.jpg',
         voice_uri: '/voice/CoBaBinhDuong.wav'
     },
     {
         id: 3,
         name: 'Web 5 ngày',
         idRepo: 'capleaf/viXTTS',
-        avatar_uri: '/image/web-5-ngay.jpg',
+        avatar_uri: 'image/web-5-ngay.jpg',
         voice_uri: '/voice/web5ngay.wav'
     },
     {
         id: 4,
         name: 'Vũ trụ nguyên thủy',
         idRepo: 'capleaf/viXTTS',
-        avatar_uri: '/image/vu-tru-nguyen-thuy.jpg',
+        avatar_uri: 'image/vu-tru-nguyen-thuy.jpg',
         voice_uri: '/voice/VuTruNguyenThuy.wav'
     }
 ]
