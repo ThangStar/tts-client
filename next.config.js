b0fcc668e3ad202b0f52f1a9ec13f3fa6c92d766
ThangStar <nguyenquocthinhit@gmail.com>
2024-11-15 11:17:12 +0700

    main

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   // basePath: "/tts-client",
   // output: "export",
+  async rewrites() {
+    return [
+      {
+        source: '/api/ttm/:path*',
+        destination: 'http://haihoanghorse.io.vn/v2/api/ttm/:path*'
+      }
+    ]
+  },
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
